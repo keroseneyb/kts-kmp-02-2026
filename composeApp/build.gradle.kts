9a9255de8f6f537450e959ba21159211863265fb
keroseneyb <keroseneyb@gmail.com>
2026-02-19 20:41:54 +0500

    Add dependency serialization

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.composeHotReload)
+    alias(libs.plugins.kotlinSerialization)
 }
 
 kotlin {
@@ -35,6 +36,7 @@ kotlin {
             implementation(libs.compose.navigation)
             implementation(libs.coil)
             implementation(libs.coil.okhttp)
+            implementation(libs.kotlin.serialization)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
