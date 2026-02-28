e20a739f2bf56950c8307b24a66c38bdbb797f44
keroseneyb <keroseneyb@gmail.com>
2026-02-28 19:54:17 +0500

    Added windowsInsets, used AppImage

--- composeApp/src/commonMain/kotlin/com/kerosene/kts_kmp_02_2026/feature/greeting/presentation/GreetingScreen.kt
+++ composeApp/src/commonMain/kotlin/com/kerosene/kts_kmp_02_2026/feature/greeting/presentation/GreetingScreen.kt
@@ -3,19 +3,20 @@ package com.kerosene.kts_kmp_02_2026.feature.greeting.presentation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
+import com.kerosene.kts_kmp_02_2026.common.ui.AppImage
 import ktskmp022026.composeapp.generated.resources.Res
 import ktskmp022026.composeapp.generated.resources.gotologin
 import ktskmp022026.composeapp.generated.resources.welcome
@@ -26,7 +27,9 @@ fun GreetingScreen(
     onGoToLoginButtonClick: () -> Unit
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.systemBars),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -40,11 +43,8 @@ fun GreetingScreen(
 
 @Composable
 private fun GreetingImage() {
-    AsyncImage(
-        model = "https://i.pinimg.com/736x/4e/5c/f7/4e5cf7d4ccb9c59b6620a9c71944d51e.jpg",
-        contentScale = ContentScale.Fit,
-        modifier = Modifier.fillMaxWidth(),
-        contentDescription = null
+    AppImage(
+        model = "https://i.pinimg.com/736x/4e/5c/f7/4e5cf7d4ccb9c59b6620a9c71944d51e.jpg"
     )
 }
 
