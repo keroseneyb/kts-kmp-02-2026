d2589bc78e3145c7271b47430b6470c073204239
keroseneyb <keroseneyb@gmail.com>
2026-03-02 00:02:33 +0500

    Added core:navigation module, appnavgraph, dependencies

---
+++ core/navigation/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kotlinSerialization)
+}
+
+kotlin {
+    androidTarget()
+    jvm()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api(libs.compose.navigation)
+                api(libs.kotlin.serialization)
+                implementation(project(":features:auth"))
+                implementation(project(":features:greeting"))
+                implementation(project(":features:feed"))
+                implementation(project(":core:common"))
+            }
+        }
+    }
+}
+
+android {
+    namespace = "com.kerosene.core.navigation"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
